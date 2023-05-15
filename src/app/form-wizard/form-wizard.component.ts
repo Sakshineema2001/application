ea6f1e336656023357ae20d9ca3b728fd3e1844f
sakshineema <sakshineema2001@gmail.com>
2023-05-15 17:23:34 +0530

    for wizard

--- src/app/form-wizard/form-wizard.component.ts
+++ src/app/form-wizard/form-wizard.component.ts
@@ -1,17 +1,18 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { EliteService } from '../elite.service';
 import { Router } from '@angular/router';
-
+import { MatStepper } from '@angular/material/stepper';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-form-wizard',
   templateUrl: './form-wizard.component.html',
   styleUrls: ['./form-wizard.component.css']
 })
-export class FormWizardComponent {
+export class FormWizardComponent implements OnInit{
 
-  formWizard !:  FormGroup;
-   
+  currentStepIndex: number = 0;
+
   public elite = {
     name : '',
     contactNo : '',
@@ -20,89 +21,103 @@ export class FormWizardComponent {
     paymentMode : '',
     date : '',
     state : {
-      id: ''
+      id: '',
+      state : ''
     },
     city : {
-      id: ''
+      id: '',
+      city:''
     },
     course : {
-     id : ''
+     id : '',
+     courseName :'',
     },
-    bankDetails : {
-      id : '',
-      accountNo : '',
-      owner : '',
-      ifsc  : ''
+    bankDetails :{
+      id :'',
+      accountNo :'',
+      owner :'',
+      ifsc :''
     }
 }
+  public city = [
+    {
+      id : '',
+      city : ''
+    }
+  ]
+   public state = [
+    {
+      id : '',
+      state : ''
+    }
+  ]
 
-public city = [
-  {
-    id : '',
-    city : ''
+  public courses = [
+   {
+       id : '',
+       courseName : ''
+   }
+  ] 
+
+  public bankDetails = [
+    {
+    id :'',
+    accountNo :'',
+    owner :'',
+    ifsc :''
   }
 ]
 
+@ViewChild('stepper') stepper !: MatStepper;
+@ViewChild('firstForm', { static: false }) firstForm!: NgForm;
+@ViewChild('secondForm', { static: false }) secondForm!: NgForm;
+  
 
- public state = [
-  {
-    id : '',
-    state : ''
+  constructor( private _formBuilder: FormBuilder, private service: EliteService,private router :Router) {}
+
+
+  ngOnInit(): void {
+    this.service.getCourses().subscribe((data:any) =>{
+      this.courses = data;
+      console.log(this.courses)
+  },(error) => {
+    alert('something went wrong')
+  })
+  this.service.getAllState().subscribe((data:any) =>{
+   this.state = data;
+  })
+
   }
-]
 
-public courses = [
- {
-     id : '',
-     courseName : ''
- }
-] 
+  onChangeState(elite: any) {
+    const id = elite.state.id;
+    if (id) {
+     this.service.getCities(id).subscribe((data :any)=> 
+       this.city = data
+     ); } 
+    }
 
-public bankDetails = [
-  {
-    id : '',
-    accountNo : '',
-    owner : '',
-    ifsc  : ''
-  }
-]
+    onNext() {
+     if (this.firstForm.valid) {
+       this.currentStepIndex++;
+       this.stepper.selectedIndex = this.currentStepIndex - 1;
+       this.stepper.next();
+      this.secondForm.resetForm();
+     } else {
+         alert('Please fill in all the required fields.');
+         this.stepper.selectedIndex = this.currentStepIndex;
+     }
+    }
 
+    formSubmit(){
+      this.service.postData(this.elite).subscribe((data) =>{
+        this.goToEliteList();
+      }, (error) =>{
+        alert('failed');
+      });
+     }
 
-constructor( @Inject(FormBuilder) _formBuilder: FormBuilder) {
-  this.formWizard = _formBuilder.group({
-    firstFormGroup: _formBuilder.group({
-          name: ['', Validators.required],
-          contactNo: ['', Validators.required],
-          course: ['', Validators.required],
-          city: ['', Validators.required],
-          state: ['', Validators.required],
-          approval: ['', Validators.required],
-          description: ['', Validators.required],
-          paymentMode: ['', Validators.required],
-          date: ['', Validators.required],
-
-      }),
-      secondFormGroup: _formBuilder.group({
-          accountNo: ['', Validators.required],
-          owner: ['', Validators.required],
-          ifsc: ['', Validators.required],
-      }),
-      thirdFormGroup: _formBuilder.group({
-        //preview of all this
-        name: ['', Validators.required],
-          contactNo: ['', Validators.required],
-          course: ['', Validators.required],
-          city: ['', Validators.required],
-          state: ['', Validators.required],
-          approval: ['', Validators.required],
-          description: ['', Validators.required],
-          paymentMode: ['', Validators.required],
-          date: ['', Validators.required],
-          accountNo: ['', Validators.required],
-          owner: ['', Validators.required],
-          ifsc: ['', Validators.required],
-    })
-  });
-}
-
+     goToEliteList(){
+      this.router.navigate(['/data-table']);
+    }
 }
